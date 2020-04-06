bf61e649f1f473ea703b1fc2bffff11cf774b523
Austin Kregel <austin@kregel.co>
2020-04-06 13:25:14 -0400

    After some thinking, the Credential model is no longer abstract as it is likely what will be used in the app.

--- src/Credential.php
+++ src/Credential.php
@@ -4,17 +4,22 @@ namespace Kregel\Basement;
 
 use Illuminate\Database\Eloquent\Model;
 
-abstract class Credential extends Model
+/**
+ * @property int $user_id
+ * @property string $name Just a pretty name for the credential in the database
+ * @property string $type just a slugified version of the name. Essentially used in a switch statement to build the services
+ * @property string $access_token Passed to services. May also be an API key, or app username
+ * @property string $refresh_token Passed to services. May also be an app password.
+ * @property boolean $enabled_on A nullable timestamp for when the user enabled the service, if it's null, it's assumed to be disabled.
+ */
+class Credential extends Model
 {
     public $fillable = [
         'user_id',
-        'name', // Just a pretty name for the credential in the database
-        'type', // just a slugified version of the name. Essentially used in a switch statement to build the services
-        'access_token', // Passed to services. May also be an API key, or app username
-        'refresh_token', // Passed to services. May also be an app password.
-        'enabled_on', // A nullable timestamp for when the user enabled the service, if it's null, it's assumed to be disabled.
+        'name',
+        'type',
+        'access_token',
+        'refresh_token',
+        'enabled_on',
     ];
-
-    abstract public function domainService(): ?DomainServiceContract;
-    abstract public function serverService(): ?ServerServiceContract;
 }
